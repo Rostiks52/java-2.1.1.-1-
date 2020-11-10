0b710d3fd51ac5c0c89e685abfe3d3569e5309cb
Rostiks52 <ksyu_05@mail.ru>
2020-11-10 22:57:36 +0300

    Files added

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+   public static void main(String[] args) {
+       double price = 98856.36;
+       int percent = 5;
+       int miles = (int) price * percent / 100;
+       System.out.println(miles);
+   }
+}
